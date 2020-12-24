f8e071ad1838625bb7c77e25785e465b1a158ffb
Luis Manuel Gerardo <luima@MacBook-Pro.local>
2020-12-24 07:14:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.querySelector("#video");
+const button = document.querySelector("#button");
+
+// Prompt to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    console.log('whoops, error here:', error)
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+})
+
+// OnLoad
+selectMediaStream();
